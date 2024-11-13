bfef183bef3d73594c6030de86383d995a4e21e7
Andres H Carrera <andreshcar@live.com>
2024-11-13 14:09:30 -0700

    fix(docs): Fixing pdoc issues that were introduced to main. (#17)
    ## Describe your changes
    
    Fixed an issue where the class I made to do the dynamic wrapping broke
    the attribute lookup with graphql.
    
    ## Issue ticket number and link
    N/a
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] I have defined what tests cover my change and or added new test to
    cover change.
    - [x] I have added comments to the change and updated any relevant docs.
    - [x] I have pulled the latest in the main branch and merged them ahead
    of time with no conflicts.

--- zombie_nomnom_api/graphql_app/schema.py
+++ zombie_nomnom_api/graphql_app/schema.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from logging import getLogger
 import os
 
@@ -8,17 +9,27 @@ from ariadne import (
     make_executable_schema,
     load_schema_from_path,
 )
+from typing import TypeVar
 from zombie_nomnom import DieColor, Face
-import zombie_nomnom_api
 
 
-logger = getLogger(__name__)
-registry: dict[str, SchemaBindable] = {}
+_logger = getLogger(__name__)
+_registry: dict[str, SchemaBindable] = {}
+
+TRegistry = TypeVar("TRegistry", bound=SchemaBindable)
 
 
-def register(graphql_type: SchemaBindable):
+def register(graphql_type: TRegistry) -> TRegistry:
+    """Adds bindable type to schema registry for the instantiation of the graphql executable schema.
+
+    **Parameters**
+    - graphql_type (SchemaBindable): The type we are registering.
+
+    **Returns**
+    - SchemaBindable: The type that was registered
+    """
     if not isinstance(graphql_type, SchemaBindable):
-        logger.warning(
+        _logger.warning(
             f"Failed to register a non bindable type, Type must implement SchemaBindable from ariadne."
         )
         return
@@ -28,53 +39,74 @@ def register(graphql_type: SchemaBindable):
             graphql_type.name
         )  # not defined in base type but all used types will have it.
     except AttributeError:
-        logger.warning(f"Unable to resolve name for schema {graphql_type}")
+        _logger.warning(f"Unable to resolve name for schema {graphql_type}")
         return
-    if key in registry:
-        logger.warning(
+    if key in _registry:
+        _logger.warning(
             f"{key} is already defined as a type skipping duplicate registration."
         )
         return
-    logger.debug(f"Registered type: {key}")
-    registry[key] = graphql_type
+    _logger.debug(f"Registered type: {key}")
+    _registry[key] = graphql_type
+    return graphql_type
+
+
+def regsiter_enum(enum_type: type[Enum], *, name: str = None):
+    """Shortcut function to register the enum type to the graphql type registry.
+
+    **Parameters**
+    - enum_type (EnumType): The enum type we want to add to graphql schema.
+    - name (str, optional): An alias name used by grapqhl to refer to the enum if the enum name is different then what is in the schema.
+    """
+    return register(EnumType(name or enum_type.__name__, enum_type))
 
 
 def build_schema():
     path_to_schema = os.path.normpath(
         os.path.join(
-            os.path.dirname(zombie_nomnom_api.__file__),
-            "graphql_app",
+            os.path.dirname(__file__),
             "schema.gql",
         ),
     )
-    logger.debug(f"Loading schema from path: {path_to_schema}")
-    logger.debug(f"Registered schemas: {list(registry.keys())}")
+    _logger.debug(f"Loading schema from path: {path_to_schema}")
+    _logger.debug(f"Registered schemas: {list(_registry.keys())}")
     raw_schema = load_schema_from_path(path_to_schema)
-    return make_executable_schema(raw_schema, *registry.values())
+    return make_executable_schema(raw_schema, *_registry.values())
 
 
-class ObjectTypeBuilder:
-    """
-    Builder for the different schema types
-    that registers itself in the global
-    map using the native ariadne ObjectType bindable.
-    """
+Query = register(ObjectType("Query"))
+"""
+Query type that is used as the entrypoint for reads in grapqhl
+"""
+Mutation = register(ObjectType("Mutation"))
+"""
+Mutation type that is used as the entrypoint for writes in grapqhl
+"""
+GameResource = register(ObjectType("Game"))
+"""
+Game type that we expose in graphql
+"""
+Round = register(ObjectType("Round"))
+"""
+Round type that we expose in graphql
+"""
+PlayerResource = register(ObjectType("Player"))
+"""
+Player type that we expose in graphql
+"""
+DieBagResource = register(ObjectType("DieBag"))
+"""
+DieBag type that we expose in graphql
+"""
+DieResource = register(ObjectType("Die"))
+"""
+Die type that we expose in graphql
+"""
+Move = register(ObjectType("Move"))
+"""
+Move type that we expose in graphql
+"""
 
-    def __init__(self, name: str) -> None:
-        self._instance = ObjectType(name)
-        register(self._instance)
-
-    def __getattr__(self, name: str):
-        return self._instance.field(name)
-
-
-Query = ObjectTypeBuilder("Query")
-Mutation = ObjectTypeBuilder("Mutation")
-GameResource = ObjectTypeBuilder("Game")
-Round = ObjectTypeBuilder("Round")
-PlayerResource = ObjectTypeBuilder("Player")
-DieBagResource = ObjectTypeBuilder("DieBag")
-DieResource = ObjectTypeBuilder("Die")
-Move = ObjectTypeBuilder("Move")
-register(EnumType("DieColor", DieColor))
-register(EnumType("DieFace", Face))
+# Register enums for GQL
+regsiter_enum(DieColor)
+regsiter_enum(Face, name="DieFace")
