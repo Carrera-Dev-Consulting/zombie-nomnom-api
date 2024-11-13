7de662ef639b65364e61e494aa061bb5614b79be
Andres H Carrera <andreshcar@live.com>
2024-11-13 16:14:07 -0700

    feat(infra): Adding docker specific files and making it runnable in containers. (#21)
    ## Describe your changes
    
    Create dockerfile and docker-compose to build and run the app and allow
    us to be able to test it within docker.
    
    ## Issue ticket number and link
    
    Closes #14 
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] I have defined what tests cover my change and or added new test to
    cover change.
    - [x] I have added comments to the change and updated any relevant docs.
    - [x] I have pulled the latest in the main branch and merged them ahead
    of time with no conflicts.

--- zombie_nomnom_api/app.py
+++ zombie_nomnom_api/app.py
@@ -13,6 +13,10 @@ from .server import fastapi_app
 
 
 class HostnameParameter(click.ParamType):
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "hostname"
+
     hostname_format = re.compile(
         r"^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$"
     )
