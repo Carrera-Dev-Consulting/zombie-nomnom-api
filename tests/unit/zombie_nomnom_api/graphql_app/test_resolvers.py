24f9effb95667fb8f1d90d836087e2e53a5c57ec
Andres H Carrera <andreshcar@live.com>
2024-11-13 00:04:27 -0700

    feat(game): Adding new routes in graphql to support starting and reading game data. (#12)
    ## Describe your changes
    
    Adding new mutation and query to view and create a game session.
    
    ## Issue ticket number and link
    
    Closes #3 
    
    ## Checklist before requesting a review
    - [x] I have performed a self-review of my code
    - [x] I have defined what tests cover my change and or added new test to
    cover change.
    - [x] I have added comments to the change and updated any relevant docs.
    - [x] I have pulled the latest in the main branch and merged them ahead
    of time with no conflicts.

--- tests/unit/zombie_nomnom_api/graphql_app/test_resolvers.py
+++ tests/unit/zombie_nomnom_api/graphql_app/test_resolvers.py
@@ -1,6 +1,76 @@
-from zombie_nomnom_api.graphql_app.resolvers import hello
+import pytest
+from zombie_nomnom_api.game import GameMaker
+from zombie_nomnom_api.graphql_app.dependencies import DIContainer
+from zombie_nomnom_api.graphql_app.resolvers import games_resolver, create_game_resolver
 
 
-def test_hello__when_resolving_value__is_french():
-    value = hello(None, None)
-    assert value == "Bonjour"
+@pytest.fixture
+def di_container() -> DIContainer:
+    return DIContainer()
+
+
+@pytest.fixture
+def game_maker():
+    return GameMaker()
+
+
+def test_games_resolver__when_given_an_id_and_game_exists__returns_list_with_game(
+    di_container: DIContainer,
+    game_maker: GameMaker,
+):
+    di_container[GameMaker] = game_maker
+    game = game_maker.make_game(["player1", "player2"])
+    games = games_resolver(None, None, id=game.id, dependencies=di_container)
+    assert len(games) == 1
+    assert games[0] == game
+
+
+def test_games_resolver__when_given_an_id_and_game_does_not_exist__returns_empty_list(
+    di_container: DIContainer,
+    game_maker: GameMaker,
+):
+    di_container[GameMaker] = game_maker
+    games = games_resolver(
+        None, None, id="This game is on fire!!", dependencies=di_container
+    )
+    assert len(games) == 0
+
+
+def test_games_resolver__when_not_given_an_id__returns_all_games(
+    di_container: DIContainer,
+    game_maker: GameMaker,
+):
+    di_container[GameMaker] = game_maker
+    game_maker.make_game(["player1", "player2"])
+    game_maker.make_game(["player3", "player4"])
+
+    games = games_resolver(None, None, dependencies=di_container)
+    assert len(games) == 2
+
+
+def test_create_game_resolver__when_no_players_provided__returns_error(
+    di_container: DIContainer,
+    game_maker: GameMaker,
+):
+    di_container[GameMaker] = game_maker
+    response = create_game_resolver(None, None, players=[], dependencies=di_container)
+
+    assert response["errors"]
+    assert response["game"] is None
+
+    assert len(list(game_maker)) == 0
+
+
+def test_create_game_resolver__when_players_provided__returns_new_game_instance(
+    di_container: DIContainer,
+    game_maker: GameMaker,
+):
+    di_container[GameMaker] = game_maker
+    response = create_game_resolver(
+        None, None, players=["Player One"], dependencies=di_container
+    )
+
+    assert response["errors"] == []
+    assert response["game"]
+
+    assert len(list(game_maker)) == 1
