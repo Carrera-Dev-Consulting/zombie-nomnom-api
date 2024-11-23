d4935ba5c3ffbbc7f404c8ca5f43a60a7dd35909
Dean-Pesek <deanapesek99@gmail.com>
2024-11-23 11:42:02 -0600

    Ran linter. Renamed get_client to get_client_mongo

--- zombie_nomnom_api/db/client.py
+++ zombie_nomnom_api/db/client.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 
-def get_client(connection_string: str) -> MongoClient:
+def get_client_mongo(connection_string: str) -> MongoClient:
     if not connection_string:
         return None
-    
+
     return MongoClient(connection_string)
