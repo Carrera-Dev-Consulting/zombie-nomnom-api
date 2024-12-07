e781eb5d28cbb1ef9f4b5a2fd15a312a7c12dada
Andres H Carrera <andreshcar@live.com>
2024-12-06 21:24:50 -0700

    chore(build): Add defaults to oauth fields (#52)
    ## Describe your changes
    
    Making OAuth Config settings get defaulted so we don't have to configure
    stuff.
    
    ## Issue ticket number and link
    
    N/A
    
    ## Checklist before requesting a review
    - [ ] I have performed a self-review of my code
    - [ ] I have defined what tests cover my change and or added new test to
    cover change.
    - [ ] I have added comments to the change and updated any relevant docs.
    - [ ] I have pulled the latest in the main branch and merged them ahead
    of time with no conflicts.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,1 @@
-import os
 
-os.environ["MONGO_CONNECTION"] = "mongodb://test_user:admin@mongo:27017/zombie_nomnom"
-os.environ["OAUTH_DOMAIN"] = "required"
-os.environ["OAUTH_ISSUER"] = "is"
-os.environ["OAUTH_AUDIENCE"] = "that"
-os.environ["OAUTH_ALGORITHMS"] = "stuff"
